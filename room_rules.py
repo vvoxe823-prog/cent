fe32a5e04c04b8867aab9befd383a4c42d14803e
vvoxe823-prog <vvoxe823@gmail.com>
2026-09-11 22:27:29 +0530

    Add files via upload

---
+++ room_rules.py
@@ -0,0 +1,14 @@
+ROOM_RULES = {
+    "bedroom": {
+        "min_width": 10,
+        "min_depth": 10
+    },
+    "living": {
+        "min_width": 10,
+        "min_depth": 12
+    },
+    "kitchen": {
+        "min_width": 7,
+        "min_depth": 8
+    }
+}
